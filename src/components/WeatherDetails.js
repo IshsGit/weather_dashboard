0e4d07c4f8ad97b7f28670421924c53af766df28
ishsgit <aishtahad@yahoo.com>
2025-01-15 12:00:00 -0500

    Add WeatherDetails.js to show pressure, humidity, wind speed, sunrise, and sunset

--- src/components/WeatherDetails.js
+++ src/components/WeatherDetails.js
@@ -1,16 +1,23 @@
 import React from 'react';
 
 function WeatherDetails({ weatherData }) {
-  const { main, wind } = weatherData;
+  const { main, wind, sys } = weatherData;
 
   return (
-    <div>
-      <h3>Additional Details</h3>
-      <p>Humidity: {main.humidity}%</p>
+    <div style={detailsStyle}>
       <p>Pressure: {main.pressure} hPa</p>
+      <p>Humidity: {main.humidity}%</p>
       <p>Wind Speed: {wind.speed} m/s</p>
+      <p>Sunrise: {new Date(sys.sunrise * 1000).toLocaleTimeString()}</p>
+      <p>Sunset: {new Date(sys.sunset * 1000).toLocaleTimeString()}</p>
     </div>
   );
 }
+
+const detailsStyle = {
+  textAlign: 'center',
+  marginTop: '20px',
+  fontSize: '1.1rem',
+};
 
 export default WeatherDetails;
