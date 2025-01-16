d44ce8b46b92df4eb9f27b1fc0d2b6bce83978e2
ishsgit <aishtahad@yahoo.com>
2025-01-16 12:00:00 -0500

    Update RecentSearches.js to display and click on recent city searches

--- src/components/RecentSearches.js
+++ src/components/RecentSearches.js
@@ -2,17 +2,33 @@ import React from 'react';
 
 function RecentSearches({ searches, onSelect }) {
   return (
-    <div>
-      <h3>Recent Searches</h3>
-      <ul>
-        {searches.map((city, index) => (
-          <li key={index} onClick={() => onSelect(city)} style={{ cursor: 'pointer' }}>
-            {city}
-          </li>
-        ))}
-      </ul>
+    <div style={searchListStyle}>
+      <h3>Recent Searches:</h3>
+      {searches.length === 0 ? (
+        <p>No recent searches</p>
+      ) : (
+        <ul>
+          {searches.map((city, index) => (
+            <li key={index} onClick={() => onSelect(city)} style={cityStyle}>
+              {city}
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
+
+const searchListStyle = {
+  textAlign: 'center',
+  marginTop: '20px',
+};
+
+const cityStyle = {
+  cursor: 'pointer',
+  padding: '5px',
+  fontSize: '1rem',
+  listStyleType: 'none',
+};
 
 export default RecentSearches;
